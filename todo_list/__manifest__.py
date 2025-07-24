393b0ea46cfec8aee845cc49a5376292c7c2229b
Omar Ahmed <o.ahmed@plementus.com>
2025-07-24 20:42:49 +0300

    first_commit

--- todo_list/__manifest__.py
+++ todo_list/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Todo List",
-    "version": "18.0",
+    "version": "17.0",
     "category": "Tools",
     'summary': 'Omar Ahmed Saeed FTCO TodoList',
     'description': '''
@@ -17,8 +17,7 @@
     ],
     "assets": {
         'web.assets_backend': [
-            'todo_list/static/src/js/client_action.js',
-            'todo_list/static/src/xml/client_action.xml',
+            'todo_list/static/src/*/**',
         ]
     },
     "license": "LGPL-3",
