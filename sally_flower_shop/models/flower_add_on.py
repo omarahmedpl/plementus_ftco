dde2f28eeda0498b1b8dd3e21bb1b0d3abeda8c8
Omar Ahmed <o.ahmed@plementus.com>
2025-07-30 14:16:18 +0300

    Initial empty commit

--- sally_flower_shop/models/flower_add_on.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class FlowerAddOn(models.Model):
-    _name = 'flower.addon'
-    _description = 'Flower Add-On'
-
-    name = fields.Char(required=True)
-    price = fields.Float(required=True)
